11de692d50c78bf43580bf2332d996234af886b7
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-08 23:53:44 +0200

    Update API.py

--- API.py
+++ API.py
@@ -28,15 +28,15 @@ df=df.iloc[:,0:]
 #Application
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('https://score-credit-api.herokuapp.com/')
 def hello_world():
     return 'Bienvenue sur le modèle de scoring'
 
-@app.route('/ID')
+@app.route('https://score-credit-api.herokuapp.com/ID')
 def ID():
     return 'ID du client'
 
-@app.route('/ID/<id>', methods=['GET'])
+@app.route('https://score-credit-api.herokuapp.com/ID/<id>', methods=['GET'])
 def Prediction(id):
     ID=int(id) #100194
     index=df[df["SK_ID_CURR"]==ID].index.values[0]
