b30ff085b67933ab8c03a05b9d10ac7409d34b86
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-08 23:54:16 +0200

    Update API.py

--- API.py
+++ API.py
@@ -2,7 +2,7 @@
 import numpy as np
 from flask import Flask, request, jsonify
 import pickle
-import requests
+#import requests
 import pandas as pd
 from flask import Flask, request
 import urllib.request as urllib2
