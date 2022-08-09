846793e979cc1a1b6fec0d32c87342ca9c85d936
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-09 19:36:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,3 +43,20 @@ def index():
         return 'YES WORK'
     else :
         return 'NO NO WORK'
+    
+@app.route('/ID')
+def ID():
+    return 'ID du client'
+
+@app.route('/ID/<id>', methods=['GET'])
+def Prediction(id):
+    ID=int(id) #100194
+    index=df[df["SK_ID_CURR"]==ID].index.values[0]
+    proba=index
+    #proba=model.predict_proba(df.iloc[index:index+1,:])[0][1]
+    return jsonify({'proba' : proba})
+
+
+
+if __name__ == '__main__':
+    app.run()
