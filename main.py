ae02e0b85fe5d4342d5aa56645438f5cacc7b48f
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-09 19:50:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def ID():
 @app.route('/ID/<id>', methods=['GET'])
 def Prediction(id):
     ID=int(id) #100194
-    #index=df[df["SK_ID_CURR"]==ID].index.values[0]
-    proba=ID
+    index=df[df["SK_ID_CURR"]==ID].index.values[0] #102616
+    proba=index
     #proba=model.predict_proba(df.iloc[index:index+1,:])[0][1]
     return jsonify({'proba' : proba})
 
