0b9654b019c7316202d2889341dd5bac16cd8bc4
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-09 03:52:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import numpy as np
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
-import requests
-import urllib.request as urllib2
+#import requests
+#import urllib.request as urllib2
 app = Flask(__name__)
 
 #Chargement Modèle
