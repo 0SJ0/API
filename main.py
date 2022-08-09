a748adad9128b064981f9cd889836a2d66512c33
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-09 04:17:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ app = Flask(__name__)
 #filename = 'model.sav'
 #model = pickle.load(open(filename, 'rb'))
 
+#Chargement dataset
+
+target_url="https://scoring-credit.s3.eu-west-3.amazonaws.com/df_Xvalidation.txt"
+data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+df = pd.read_csv(data, sep=',',index_col=0)
+df=df.reset_index(drop=True)
+df=df.iloc[:,0:]
+
 
 @app.route('/')
 def index():
