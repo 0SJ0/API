5452501a46d229030a6906ec4fe0517a9c56231c
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-09 04:14:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
 import requests
-#import urllib.request as urllib2
+import urllib.request as urllib2
 app = Flask(__name__)
 
 #Chargement Modèle
-filename = 'model.sav'
-model = pickle.load(open(filename, 'rb'))
+#filename = 'model.sav'
+#model = pickle.load(open(filename, 'rb'))
 
 
 @app.route('/')
